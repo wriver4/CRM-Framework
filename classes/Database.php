31b7755e1e570c9e99eff49fdc93048f9691f61a
wriver4 <wriver4@gmail.com>
2025-07-31 16:13:40 -0400

    add ignore and update dbclass

--- classes/Database.php
+++ classes/Database.php
@@ -14,23 +14,10 @@ class Database
     public function __construct()
     {
         // server database connection information
-        $this->wr_host = 'localhost';
-        $this->wr_database = 'serverw_serverw';
-        $this->wr_username = 'serverw_serverw';
-        $this->wr_password = 'TJza3DrfUZS0';
-
-
-        // admin database connection information
-        $this->admin_host = 'localhost';
-        $this->admin_database = 'adminrnd_primary'; //adminrnd_primary fY5JTVs38CPW
-        $this->admin_username = 'adminrnd_primary';
-        $this->admin_password = 'fY5JTVs38CPW';
-
-        // secure connection information encrypted
-        $this->secure_host = 'localhost';
-        $this->secure_database = 'newwgadmin_secure';
-        $this->secure_username = 'root';
-        $this->secure_password = 'root';
+        $this->crm_host = 'localhost';
+        $this->crm_database = 'democrm_democrm';
+        $this->crm_username = 'democrm_democrm';
+        $this->crm_password = 'b3J2sy5T4JNm60';
 
         $this->character_set = 'utf8mb4';
         $this->options = [
@@ -39,47 +26,19 @@ class Database
             \PDO::ATTR_EMULATE_PREPARES   => false,
         ];
     }
-    public function dbwr()
+    public function crm()
     {
-        static $DBWR = null;
-        if (is_null($DBWR)) {
-            $dsn = 'mysql:host=' . $this->wr_host . ';dbname=' . $this->wr_database . ';charset=' . $this->character_set;
+        static $CRM = null;
+        if (is_null($CRM)) {
+            $dsn = 'mysql:host=' . $this->crm_host . ';dbname=' . $this->crm_database . ';charset=' . $this->character_set;
             try {
-                $pdo = new \PDO($dsn, $this->wr_username, $this->wr_password, $this->options);
+                $pdo = new \PDO($dsn, $this->crm_username, $this->crm_password, $this->options);
             } catch (\PDOException $e) {
                 throw new \PDOException($e->getMessage(), (int)$e->getCode());
             }
-            $DBWR = $pdo;
+            $CRM = $pdo;
         }
-        return $DBWR;
+        return $CRM;
     }
-    public function dbadmin()
-    {
-        static $DBADMIN = null;
-        if (is_null($DBADMIN)) {
-            $dsn = 'mysql:host=' . $this->admin_host . ';dbname=' . $this->admin_database . ';charset=' . $this->character_set;
-            try {
-                $pdo = new \PDO($dsn, $this->admin_username, $this->admin_password, $this->options);
-            } catch (\PDOException $e) {
-                throw new \PDOException($e->getMessage(), (int)$e->getCode());
-            }
-            $DBADMIN = $pdo;
-        }
-        return $DBADMIN;
-    }
-
-    public function dbsecure()
-    {
-        static $DBS = null;
-        if (is_null($DBS)) {
-            $dsn = 'mysql:host=' . $this->secure_host . ';dbname=' . $this->secure_database . ';charset=' . $this->character_set;
-            try {
-                $pdo = new \PDO($dsn, $this->secure_username, $this->secure_password, $this->options);
-            } catch (\PDOException $e) {
-                throw new \PDOException($e->getMessage(), (int)$e->getCode());
-            }
-            $DBS = $pdo;
-        }
-        return $DBS;
-    }
+    
 }
